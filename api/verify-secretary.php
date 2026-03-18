d24ad5ab55275ea0727d1c247f44072c74dde652
zenieveva-rgb <zenieveva@gmail.com>
2026-03-19 07:55:55 +0800

    Update verify-secretary.php

--- api/verify-secretary.php
+++ api/verify-secretary.php
@@ -1,1 +1,8 @@
 
+<?php
+require_once '../config.php';
+header('Content-Type: application/json');
+echo json_encode([
+    'authorized' => isset($_SESSION['user_id']) && $_SESSION['role'] === 'secretary'
+]);
+?>
