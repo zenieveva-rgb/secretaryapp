2d1a08084234155189cf9c187da5673e8cca61b1
zenieveva-rgb <zenieveva@gmail.com>
2026-03-19 08:12:38 +0800

    Delete api directory

--- api/verify-secretary.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-require_once '../config.php';
-header('Content-Type: application/json');
-echo json_encode([
-    'authorized' => isset($_SESSION['user_id']) && $_SESSION['role'] === 'secretary'
-]);
-?>
